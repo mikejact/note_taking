af36a6d93d2dcd91abba98c119b70de285de7c30
Mike <michaeljohnacton@gmail.com>
2021-04-08 18:03:18 -0500

    firsst commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BorderColorIcon from '@material-ui/icons/BorderColor';
+
+function Header() {
+  return (
+    <header>
+    
+      <h1> <BorderColorIcon /> Take a note...</h1>
+    </header>
+  );
+}
+
+export default Header;
